a9a509c49e0c9875cb89257469e73747b93321cc
Niklas Eliason <niklas.eliason@decisionbyheart.com>
2022-06-10 09:52:06 +0200

    fix some styling

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import TodoList from "./components/TodoList";
 function App() {
 	return (
 		<main className="w-screen h-screen md:p-12 p-2 mx-auto text-gray-100 bg-gradient-to-tr from-blue-700 to-purple-400 ">
-			<div className="todo-app">
-				<h1 className="text-2xl mb-4 font-extrabold mx-auto w-fit">Todo app</h1>
+			<div className="todo-app mx-auto flex-initial flex-col items-center min-w-fit">
+				<h1 className="text-4xl text-center tracking-wider mb-4 font-extrabold drop-shadow-xl">
+					Todo app
+				</h1>
 				<TodoList />
 			</div>
 		</main>
